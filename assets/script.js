17d650e2aa4084d249e334b7da22acb2c2cb0847
suzieguillermic <suzie.guillermic@hotmail.fr>
2024-10-11 09:43:20 +0200

    fin

--- assets/script.js
+++ assets/script.js
@@ -22,20 +22,15 @@ console.log(slides.length);
 // Ici on déclare la contante pour les fleches et on récupère le document déclarer dans css
 const leftArrow = document.querySelector('.arrow_left');
 const rightArrow = document.querySelector('.arrow_right');
-
-// on ajoute le addevent listener à chaque fleche 
-leftArrow.addEventListener('click', function() {
-    console.log('Left arrow clicked');
-   // on verifie avec console log 
-});
-
-rightArrow.addEventListener('click', function() {
-    console.log('Right arrow clicked');
-   
-});
-
 // on décalare la varaible dots caroussel pour les points et on recupère le documents dots dans le css
 const dotsCarroussel = document.querySelector('.dots');
+const bannerImg = document.querySelector('.banner-img');
+const bannerTagline = document.querySelector('#banner p');
+
+
+// définir la 1ére diapo sur index o du tableau
+let slidePosition = 0;
+updateSlide();
 
 // on utilise la boucle foreach pour chaque slide du tableau et son index
 slides.forEach((slide, index) => {
@@ -47,3 +42,56 @@ slides.forEach((slide, index) => {
     }
 	dotsCarroussel.appendChild(dot); // on utilise la fonction appenchild pour afficher le sapn dans le carroussel
 });
+
+
+// Fonction pour mettre à jour l'affichage de la diapositive
+function updateSlide() {
+    // Mettre à jour l'image et le texte de la bannière
+    bannerImg.src = `./assets/images/slideshow/${slides[slidePosition].image}`; // Utiliser slidePosition
+    bannerTagline.innerHTML = slides[slidePosition].tagLine; // Utiliser slidePosition
+
+    // Mettre à jour les points (dots)
+    document.querySelectorAll('.dot').forEach((dot, index) => {
+        if (index === slidePosition) { // Utiliser slidePosition
+            dot.classList.add('dot_selected');
+        } else {
+            dot.classList.remove('dot_selected');
+        }
+    });
+}
+
+
+
+
+// on ajoute le addevent listener à chaque fleche 
+leftArrow.addEventListener('click', function() {
+    console.log('Left arrow clicked');
+   // on verifie avec console log 
+   if (slidePosition === 0) {
+	// Si on est à la première image, revenir à la dernière
+	console.log('Première image, retour à la dernière');
+	slidePosition = slides.length - 1;
+} else {
+	// Sinon, reculer à l'image précédente
+	console.log('Retour à l\'image précédente');
+	slidePosition--;
+}
+updateSlide();
+});
+
+rightArrow.addEventListener('click', function() {
+    console.log('Right arrow clicked');
+	if (slidePosition === slides.length - 1) {
+        // Si on est à la dernière image, revenir à la première
+        console.log('Dernière image, retour à la première');
+        slidePosition = 0;
+    } else {
+        // Sinon, avancer à l'image suivante
+        console.log('Passage à l\'image suivante');
+        slidePosition++;
+    }
+    updateSlide();
+});
+
+
+
